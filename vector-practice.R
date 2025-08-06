de66a54bf79899305f0335298222e26ffaa91f5e
SRKads1998 <kadonoffs@gmail.com>
2025-08-06 13:58:12 -0700

    vector practice

---
+++ vector-practice.R
@@ -0,0 +1,24 @@
+library(tidyverse)
+# create and store the vector
+
+marmots <- c("blue", "green", 4, "yellow")
+
+#check the class
+class(marmots)
+
+# create and store the vector
+pika <- c(12.4, 6.8, 2.9, 8.8, 5)
+
+#check the class
+class(pika)
+
+#create the interger vector
+bear <- c(20L, 3L, 5L, 18L, 23L)
+#check the bear
+class(bear)
+# bear an interger, numbers can be stored in R in different ways, Floats(numbers with decimals), intergers etc
+
+# create two vectors
+ringtail <- c(4.3, 8.9, 2.6, 7.1)
+fox <- c(9.0, 12.5, 5.4, 10.9)
+
